e10151f7dcfb1fd8e67aad9f6fdf33d5a47bc6f3
timesnewronann <ronanborja26@gmail.com>
2026-07-27 22:23:32 -0700

    Working on Blocktype function

--- src/block.py
+++ src/block.py
@@ -1,6 +1,31 @@
+from enum import Enum
 
 
-# takes a raw markdown string (representing a full document) -> returns a list of "block" strings
+class BlockType(Enum):
+    paragraph = "paragraph"
+    heading = "heading"
+    code = "code"
+    quote = "quote"
+    unordered_list = "unordered_list"
+    ordered_list = "ordered_list"
+
+
+def block_to_block_type(markdown):
+    # takes a single block of makrdown text as input and returns the BlockType representing the type of block it is.
+    # You can assume all leading and trailing whitespace were already stripped
+
+    # takes a raw markdown string (representing a full document) -> returns a list of "block" strings
+
+    # headings might be the easiest
+
+    # Is this a heading -> answer: how many # characters (if any) are at the very start of the string and is there a space right after them?
+
+    # use a loop that tires prefix lengths 1 - 6, slicing markdown[0:n]
+    for length in range(1, 7):
+        # at each length is the slice all # characters, and the very next character a space?
+        markdown[0:length]
+
+
 def markdown_to_blocks(markdown):
     # 1. split the markdown string into pieces whenever there's a blank line separating them
     # Use .split("\n\n")
@@ -20,4 +45,3 @@ def markdown_to_blocks(markdown):
             cleaned_blocks.append(stripped_block)
 
     return cleaned_blocks
-
